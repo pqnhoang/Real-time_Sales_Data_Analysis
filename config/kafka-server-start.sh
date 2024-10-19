709f541892f3dd9f0a1637696d1d345cd0f8858e
Nguyen Hoang <hoang.pqn2123@gmail.com>
2024-10-19 20:39:15 +0700

    bash.sh

---
+++ config/kafka-server-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo ~/kafka_2.12-3.8.0/bin/kafka-server-start.sh ~/kafka_2.12-3.8.0/config/server.properties
---
+++ config/zookeeper-server-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo ~/kafka_2.12-3.8.0/bin/zookeeper-server-start.sh ~/kafka_2.12-3.8.0/config/zookeeper.properties
